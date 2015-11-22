9bc2f81ecec549cd20ade5a2bdd0d58af435cf1c
Ryan Muller <rkennedy@kennship.com>
2015-11-22 16:18:49 -0500

    Extracts `d` attribute from paths

--- Svg2Gcode.hs
+++ Svg2Gcode.hs
@@ -1,15 +1,24 @@
 module Main where
 
 import Control.Arrow
-import Text.XML.HXT.Arrow.ReadDocument (readDocument)
-import Text.XML.HXT.Arrow.WriteDocument (writeDocument)
-import Text.XML.HXT.Arrow.XmlState (withIndent)
-import Text.XML.HXT.Arrow.XmlState.RunIOStateArrow (runX)
+import Text.XML.HXT.Core
+
+data PathInstruction = Move Double Double Double
+                     | StartPath
+                     | EndPath
+
+getDoc = readDocument [] ""
 
 main :: IO ()
 main = do
-  runX $
-    readDocument [] ""
-    >>>
-    writeDocument [withIndent True] ""
-  return ()
+  result <- runX (getDoc >>> selectAllPaths)
+  case result of
+    [] -> putStrLn "No paths found"
+    p:_ -> putStrLn (take 30 p)
+
+selectAllPaths :: ArrowXml a => a XmlTree String
+selectAllPaths = deep
+                 ( isElem >>> hasName "path"
+                   >>>
+                   getAttrValue "d"
+                 )
